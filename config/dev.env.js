d0cb7839d0ab4cf9a6aae9510af4b84d5f082942
Darshan Parbadiya <darshanparbadiya@gmail.com>
2025-10-02 13:51:40 -0400

    changed prod settings

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8201/mall-admin"',
-  // BASE_API: '"http://64.23.253.84:8201/mall-admin"',
+  // BASE_API: '"http://localhost:8201/mall-admin"',
+  BASE_API: '"https://apis.darshanparbadiya.site/mall-admin"',
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://64.23.253.84:8201/mall-admin"'
+  BASE_API: '"https://apis.darshanparbadiya.site/mall-admin"'
 }
